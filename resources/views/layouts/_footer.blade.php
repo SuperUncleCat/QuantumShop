07b2938fd2d4eee246477943010a77578a93cbea
SuperUncleCat <superunclecat@gmail.com>
2018-08-31 08:13:47 +0000

    收货列表

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            created with <span style="color: #e27575;font-size: 14px;">❤</span> by h-zhang
+        </p>
+
+        <p class="pull-right"><a href="mailto:admin@quantum.com">联系我们</a></p>
+    </div>
+</footer>
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/','PagesController@root')->name('root');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware'=>'auth'],function(){
+  Route::get('/email_verified_notice','PagesController@emailVerifyNotice')->name('email_verified_notice');
+  Route::get('/email_verification/verify','EmailVerificationController@verify')->name('email_verification.verify');
+  Route::get('/email_verification/send','EmailVerificationController@send')->name('email_verification.send');
+  Route::group(['middleware'=>'email_verified'],function(){
+    Route::get('user_addresses','UserAddressesController@index')->name('user_addresses.index');
+    Route::get('user_addresses/create','UserAddressesController@create')->name('user_addresses.create');
+  });
+});
