c08190eaf8b6e0ed3a3a5f53ce6f10076d74578e
h-zhang <h-zhang@user-no-MacBook-Air.local>
2018-09-12 17:04:47 +0900

    商品模型

--- app/Admin/Controllers/UsersController.php
+++ app/Admin/Controllers/UsersController.php
@@ -35,13 +35,13 @@ class UsersController extends Controller
      * @param Content $content
      * @return Content
      */
-    // public function show($id, Content $content)
-    // {
-    //     return $content
-    //         ->header('Detail')
-    //         ->description('description')
-    //         ->body($this->detail($id));
-    // }
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('Detail')
+            ->description('description')
+            ->body($this->detail($id));
+    }
 
     /**
      * Edit interface.
@@ -50,13 +50,13 @@ class UsersController extends Controller
      * @param Content $content
      * @return Content
      */
-    // public function edit($id, Content $content)
-    // {
-    //     return $content
-    //         ->header('Edit')
-    //         ->description('description')
-    //         ->body($this->form()->edit($id));
-    // }
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('Edit')
+            ->description('description')
+            ->body($this->form()->edit($id));
+    }
 
     /**
      * Create interface.
@@ -64,13 +64,13 @@ class UsersController extends Controller
      * @param Content $content
      * @return Content
      */
-    // public function create(Content $content)
-    // {
-    //     return $content
-    //         ->header('Create')
-    //         ->description('description')
-    //         ->body($this->form());
-    // }
+    public function create(Content $content)
+    {
+        return $content
+            ->header('Create')
+            ->description('description')
+            ->body($this->form());
+    }
 
     /**
      * Make a grid builder.
@@ -125,37 +125,37 @@ class UsersController extends Controller
      * @param mixed   $id
      * @return Show
      */
-    // protected function detail($id)
-    // {
-    //     $show = new Show(User::findOrFail($id));
-    //
-    //     $show->id('Id');
-    //     $show->name('Name');
-    //     $show->email('Email');
-    //     $show->password('Password');
-    //     $show->remember_token('Remember token');
-    //     $show->email_verified('Email verified');
-    //     $show->created_at('Created at');
-    //     $show->updated_at('Updated at');
-    //
-    //     return $show;
-    // }
+    protected function detail($id)
+    {
+        $show = new Show(User::findOrFail($id));
+
+        $show->id('Id');
+        $show->name('Name');
+        $show->email('Email');
+        $show->password('Password');
+        $show->remember_token('Remember token');
+        $show->email_verified('Email verified');
+        $show->created_at('Created at');
+        $show->updated_at('Updated at');
+
+        return $show;
+    }
 
     /**
      * Make a form builder.
      *
      * @return Form
      */
-    // protected function form()
-    // {
-    //     $form = new Form(new User);
-    //
-    //     $form->text('name', 'Name');
-    //     $form->email('email', 'Email');
-    //     $form->password('password', 'Password');
-    //     $form->text('remember_token', 'Remember token');
-    //     $form->switch('email_verified', 'Email verified');
-    //
-    //     return $form;
-    // }
+    protected function form()
+    {
+        $form = new Form(new User);
+
+        $form->text('name', 'Name');
+        $form->email('email', 'Email');
+        $form->password('password', 'Password');
+        $form->text('remember_token', 'Remember token');
+        $form->switch('email_verified', 'Email verified');
+
+        return $form;
+    }
 }
