9a3e506fe913c8e800bcb32dfb2a609a36cda9a2
h-zhang <h-zhang@user-no-MacBook-Air.local>
2018-09-13 14:51:54 +0900

    用户界面 - 商品列表

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,6 @@ Route::group(['middleware'=>'auth'],function(){
     Route::delete('user_addresses/{user_address}','UserAddressesController@destroy')->name('user_addresses.destroy');
   });
 });
+
+Route::redirect('/','/products')->name('root');
+Route::get('products','ProductsController@index')->name('products.index');
